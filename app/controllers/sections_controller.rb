8d23f4747f14e520a78933cdc1054a2507e1a4a3
waders92 <wade.carlson@pga.com>
2020-08-20 04:21:14 -0500

    changes

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,10 +1,23 @@
 class SectionsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :admin_required
 
   def create
     @post = Post.find(params[:post_id])
     @post.sections.create(section_params.merge(user: current_user))
     flash.alert = 'Section was addded!'
+    redirect_to edit_post_path(@post)
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @section = Section.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @section = Section.find(params[:id])
+    @section.update(section_params)
+    flash.alert = 'Section was updated!'
     redirect_to edit_post_path(@post)
   end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :sections, dependent: :destroy
+
+  def post_topics
+    return ['Angular', 'Rails', 'JavaScript', 'MongoDB', 'ExpressJS', 'NodeJS']
+  end
 end
