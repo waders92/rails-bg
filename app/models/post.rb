244c03ac19e827c9b87f9ca864be3096d4cc8596
waders92 <wade.carlson@pga.com>
2020-08-24 14:45:55 -0500

    starting to port over styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,12 @@ class Post < ApplicationRecord
   def post_topics
     return ['Angular', 'Rails', 'JavaScript', 'MongoDB', 'ExpressJS', 'NodeJS']
   end
+
+  def first_section_content
+    return self.sections[0].content
+  end
+
+  def post_date
+    created_at.strftime('%b %d, %Y')
+  end
 end
