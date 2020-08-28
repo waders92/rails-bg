9fe6b59952af8c7242ba821f20cbc5d7869d2a51
waders92 <wade.carlson@pga.com>
2020-08-28 13:47:54 -0500

    update section order

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user, optional: true
-  has_many :sections, dependent: :destroy
+  has_many :sections, dependent: :destroy, :order => 'created_at ASC'
   has_many :images, dependent: :destroy
 
   def post_topics
