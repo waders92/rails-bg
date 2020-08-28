05934a8b9c2e484e648199505de0ab4cb78f76dc
waders92 <wade.carlson@pga.com>
2020-08-28 12:27:48 -0500

    updating gem config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,13 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        
+
+  config.storage     = :aws
+  config.aws_bucket  = ENV["AWS_BUCKET"] 
+  config.aws_acl     = :public_read
+
   config.fog_credentials = {
     provider:              'AWS',                        
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]              
+              
 end
