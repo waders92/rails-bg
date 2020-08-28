72f256926c3201f9d49d069d0af2669112ee851e
waders92 <wade.carlson@pga.com>
2020-08-28 12:54:46 -0500

    adding region to config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,8 @@ CarrierWave.configure do |config|
 
   config.aws_credentials = {                       
     access_key_id:      ENV["AWS_ACCESS_KEY"],        
-    secret_access_key:  ENV["AWS_SECRET_KEY"],        
+    secret_access_key:  ENV["AWS_SECRET_KEY"],
+    region:             ENV['AWS_REGION']        
   }
 
 end
