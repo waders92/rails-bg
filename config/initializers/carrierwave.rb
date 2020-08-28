53aa8222df522f60ae97f90f58605f6d8e60d761
waders92 <wade.carlson@pga.com>
2020-08-28 13:02:17 -0500

    updating config again

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,12 @@ CarrierWave.configure do |config|
 
   config.storage     = :aws
   config.aws_bucket  = ENV["AWS_BUCKET"] 
-  config.aws_acl     = :public_read
+  config.aws_acl     = 'private'
 
   config.aws_credentials = {                       
     access_key_id:      ENV["AWS_ACCESS_KEY"],        
     secret_access_key:  ENV["AWS_SECRET_KEY"],
-    region:             ENV['AWS_REGION']        
+    region:             ENV["AWS_REGION"]        
   }
 
 end
